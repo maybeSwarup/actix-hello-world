a57f6b0a02fa9d8c90877b02e04b5e3c64a9b7bb
Swarup Suryawanshi <suryawanshiswarup@gmail.com>
2024-06-22 14:28:03 +0530

    route scope

--- src/main.rs
+++ src/main.rs
@@ -18,6 +18,13 @@ async fn index() -> impl Responder {
     "Hello world!"
 }
 
+#[get("/show")]
+async fn show_users() -> impl Responder {
+    let users = vec!["Gangadhar", "Shaktimaan"];
+    let body = serde_json::to_string(&users).unwrap();
+    HttpResponse::Ok().body(body)
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
@@ -26,6 +33,9 @@ async fn main() -> std::io::Result<()> {
             .service(echo)
             .route("/hey", web::get().to(manual_hello))
             .service(web::scope("/app").route("index.html", web::get().to(index)))
+            .service(
+                web::scope("/users").service(show_users)
+            )
     })
     .bind(("127.0.0.1", 8080))?
     .run()
